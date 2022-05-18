23414a43cad7d00d81740a94696d5476a0753ec7
Student Help1 <STUDENTHELP1@etsu.edu>
2022-05-18 16:13:08 -0400

    validation broke, took it out -Cameron

--- Number_Game/Number_Game.cpp
+++ Number_Game/Number_Game.cpp
@@ -8,8 +8,9 @@ int main()
 {
     srand(time(0));// giving seed of time(0). This helps make sure the umber is random each time
 
-    int ranNum = (rand() % 100) + 1;//generate a number beween 1 and 100
+    int ranNum = rand() % 100 + 1;//generate a number beween 1 and 100
     int userAnswer = 0;
+    int userAttempts = 0;
 
     cout << "Welcome to the Random Number Game!\n";
 
@@ -18,25 +19,20 @@ int main()
         cout << "\nEnter a number between 1 and 100: ";
         cin >> userAnswer;
 
-        while (!(cin >> userAnswer) || (userAnswer == double(userAnswer)))//validation for user input, check if answer is an integer.
-        {
-            cout << "\nWrong input! Please enter your number again: ";
-            cin.clear();
-            cin.ignore();
-            cin >> userAnswer;
-        }
-
         if (userAnswer < ranNum)
         {
             cout << "\nYour answer is too low, try again!\n";
+            userAttempts++;
         }
         else if (userAnswer > ranNum)
         {
             cout << "\nYour answer is too big, try again!\n";
+            userAttempts++;
         }
-    }
+    };
 
     cout << "\nYou guessed the number correctly!\n";
-    cout << "Congradulations!";
+    cout << "\nCONGRATS!\n\n";
+    cout << "It took you " << userAttempts << " attempts to guess correctly";
     return 0;
 }
